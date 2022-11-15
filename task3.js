9d4a1467bb5da762b135d21c0ae3867c7d4b8c65
Alisa <alisakovalhome@mail.ru>
2022-11-15 17:01:24 +0300

    Add task3

---
+++ task3.js
@@ -0,0 +1,49 @@
+function colorBackChange() {
+    switch(document.getElementById('colorBack').value) {
+        case 'red': document.getElementById('result').style.backgroundColor = 'red'; break;
+        case 'blue': document.getElementById('result').style.backgroundColor = 'blue'; break;
+        case 'green': document.getElementById('result').style.backgroundColor = 'green'; break;
+        case 'yellow': document.getElementById('result').style.backgroundColor = 'yellow'; break;
+        case 'pink': document.getElementById('result').style.backgroundColor = 'pink'; break;
+    }
+
+
+
+    // if (document.getElementById('colorBack').value == 'red') {
+    //     document.getElementById('result').style.backgroundColor = 'red'
+    // }
+}
+
+function colorButton() {
+    document.getElementById('button').style.backgroundColor = 'red'
+}
+
+function colorButtonNew() {
+    document.getElementById('button').style.backgroundColor = 'green'
+}
+
+function textButton() {
+    let button = document.getElementById('button');
+    button.onclick = sayWhatIAm;
+    
+    function sayWhatIAm() {
+        alert(this.innerHTML);
+    }
+}
+textButton()
+
+function newColorAlert() {
+    let button = document.getElementById('button')
+
+    function borderWidth() {
+        button.style.borderWidth = '5px'
+    }
+    
+    function borderColor() {
+        button.style.borderColor = 'blue'
+    }
+    
+    button.addEventListener('click', borderWidth);
+    button.addEventListener('click', borderColor)
+}
+newColorAlert() 
