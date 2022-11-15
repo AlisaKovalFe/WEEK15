f32cce946bf620d2e6ed1e510eaff2f0cb935ca5
Alisa <alisakovalhome@mail.ru>
2022-11-15 18:51:49 +0300

    Add some answers in test

---
+++ index.js
@@ -0,0 +1,32 @@
+let date = new Date();
+let time = date.getHours();
+
+// if (time < 10) {
+//     alert('Доброе утро!');
+// } else {
+//     alert('Добрый день!')
+// }
+
+// (time < 10) ? alert('Доброе утро!') : alert('Добрый день!');
+
+if ('1') {
+    alert('Hi')
+} else {
+    alert('bye')
+}
+
+// x = 6
+// y = 3
+// if (x < 10 && y > 1) {
+//     alert('yeap')
+// }
+
+// if (x == 5 || y == 5) {
+//     alert('yeap')
+// } else {
+//     alert('no')
+// }
+
+// if (!(x == y)) {
+//     alert('yeap')
+// }
